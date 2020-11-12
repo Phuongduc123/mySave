9a8c9a921ab37d0470757c21ea61b3beaf0a4b54
Tuan Thanh <atonggiang0410@gmail.com>
2020-11-12 10:28:16 +0700

    adding api: search by username

--- rest/app/post/urls.py
+++ rest/app/post/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
-from .views import PostPageList, UserPostList, SearchPostList
+from .views import PostPageList, UserPostList, SearchPostListByTitle, SearchPostListByUser
 from django.views.decorators.cache import cache_page
 
 urlpatterns = [
     url(r'^postpage/$', cache_page(15 * 60)(PostPageList.as_view())),
+    # url(r'^postpage/$', PostPageList.as_view()),
     url(r'^userpost/$', cache_page(15 * 60)(UserPostList.as_view())),
-    url(r'^searchpost/$', cache_page(15 * 60)(SearchPostList.as_view())),
+    url(r'^searchpost/$', cache_page(15 * 60)(SearchPostListByTitle.as_view())),
+    url(r'^searchpostbyuser/$', cache_page(15 * 60)(SearchPostListByUser.as_view())),
 ]
