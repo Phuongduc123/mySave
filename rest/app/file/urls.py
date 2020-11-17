601da604cd448474e280036782144bf22bf95242
phuongnd <phuongceo13@gmail.com>
2020-11-17 13:30:13 +0700

    search-by-author

--- rest/app/file/urls.py
+++ rest/app/file/urls.py
@@ -3,6 +3,6 @@ from .views import UserFileList, GetFile
 from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    url(r'^userfile/$', cache_page(60 * 60)(UserFileList.as_view())),
+    url(r'^userfile/$', UserFileList.as_view()),
     url(r'^getfile/$', cache_page(15 * 60)(GetFile.as_view())),
 ]
